57772ea00e5c53dc2eb0d87d2ccfc53ff52d406f
YashasBNB <yashasnaidu3@gmail.com>
2024-02-22 11:49:40 +0530

    done

---
+++ react/src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+
+import React from 'react';
+import './App.css'; // You can create this file for styling
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to ExploreVista</h1>
+        <p>
+          This is a simple landing page for my ASAP peoject called ExploreVista.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
