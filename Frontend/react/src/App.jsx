2f2241ec5bfa232d122664ea84684ad122fd5ba2
YashasBNB <yashasnaidu3@gmail.com>
2024-02-26 15:56:45 +0530

    done

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,17 +1,19 @@
 // src/App.jsx
-
 import React from 'react';
-import './App.css'; // You can create this file for styling
-
+import './App.css';
+import {Routes,Route, Link } from 'react-router-dom';
+import Forms from './components/Signup'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to ExploreVista</h1>
-        <p>
-          This is a simple landing page for my ASAP peoject called ExploreVista.
-        </p>
-      </header>
+      <h1>Welcome to ExploreVista</h1>
+      <p>This is a simple landing page for my ASAP project called ExploreVista.</p>
+      <Link to="/Signup">
+       <button>Sign Up</button> 
+      </Link>
+      <Routes>
+        <Route path="/Signup" element={<Forms/>}></Route>
+      </Routes>
     </div>
   );
 }
