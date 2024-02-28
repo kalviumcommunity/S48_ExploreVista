b9b9cbcc12bd57dbf0575061058bb84249314a47
YashasBNB <yashasnaidu3@gmail.com>
2024-02-28 18:00:23 +0530

    done

--- Frontend/react/src/App.jsx
+++ Frontend/react/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {Routes,Route, Link } from 'react-router-dom';
 import Forms from './components/Signup'
 import  Log from './components/Loginpage'
 import Navbar from './Navbar';
 import Home from './components/Home';
+import axios from 'axios'; 
+import { useEffect } from 'react';
 
 function App() {
+  const [users,setUsers] = useState([])
+   useEffect(() => {
+    axios.get('http://localhost:3000/')
+    .then(users => setUsers(users.data))
+    .catch(err => console.log(err))
+   },[])
   return (
     <div className="App">
 <header>
@@ -17,6 +25,20 @@ function App() {
         <Route path="/Signup" element={<Forms/>}></Route>
         <Route path="/Home" element={<Home />}></Route>
       </Routes>
+      <div className="api">
+      {users &&
+          users.map((guides, id) => (
+            <div className="data" key={id}>
+              <p><strong>Place: </strong>{guides.place}</p>
+              <p><strong>Vehicles Available: </strong>{guides.vehicles_available}</p>
+              <p><strong>Options to Stay: </strong>{guides.options_to_stay}</p>
+              <p><strong>Brief History: </strong>{guides.brief_history}</p>
+              <p><strong>Best Time to Visit: </strong>{guides.best_time_to_visit}</p>
+
+            </div>
+          ))}
+        
+      </div>
     </div>
   );
 }
--- Frontend/react/src/components/Home.jsx
+++ Frontend/react/src/components/Home.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Navbar from '../Navbar';
+import axios from 'axios'; 
+import { useEffect } from 'react';
 
 function Home(){
+    const [users,setUsers] = useState([])
+    useEffect(() => {
+     axios.get('http://localhost:3000/')
+     .then(users => setUsers(users.data))
+     .catch(err => console.log(err))
+    },[])
     return(
-        <header>
-            
-        </header>
+        <div className="api">
+        {users &&
+            users.map((guides, id) => (
+              <div className="data" key={id}>
+                <p><strong>Place: </strong>{guides.place}</p>
+                <p><strong>Vehicles Available: </strong>{guides.vehicles_available}</p>
+                <p><strong>Options to Stay: </strong>{guides.options_to_stay}</p>
+                <p><strong>Brief History: </strong>{guides.brief_history}</p>
+                <p><strong>Best Time to Visit: </strong>{guides.best_time_to_visit}</p>
+  
+              </div>
+            ))}
+          
+        </div>
         
     )
 }
